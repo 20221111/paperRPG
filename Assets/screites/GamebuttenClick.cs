e21e83923115ec9a8965d4c4e11c03357e259509
nmy6452 <85818940+nmy6452@users.noreply.github.com>
2022-02-16 00:21:38 +0900

    error normalization

---
+++ Assets/screites/GamebuttenClick.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamebuttenClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnclickButton()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        
+    }
+}
